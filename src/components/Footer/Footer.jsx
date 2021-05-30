2e22302f148098fb7bbbe63dee411b159ed1adcc
sakkarakaya <sakkarakaya@icloud.com>
2021-05-30 17:57:59 +0200

    edited footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,9 +38,9 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+          © {new Date().getFullYear()} - Powered by{' '}
+          <a href="https://github.com/sakkarakaya" target="_blank" rel="noopener noreferrer">
+            Mehmet S. Karakaya
           </a>
         </p>
 
